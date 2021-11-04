4800c5e85278eff8d7c7a89b59c028c3e1e800fc
Deploy from CI <>
2021-11-04 16:16:45 +0000

    Deploy 3bde54ffef10bc05c0f3d6498d5a42eabfa4bee9 to gh-pages

---
+++ x86_64-pc-windows-msvc/doc/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["libc"] = {"name":"","dirs":[{"name":"windows","dirs":[{"name":"msvc","dirs":[{"name":"x86_64","files":["align.rs","mod.rs"]}],"files":["mod.rs"]}],"files":["mod.rs"]}],"files":["fixed_width_ints.rs","lib.rs","macros.rs"]};
+createSourceSidebar();
